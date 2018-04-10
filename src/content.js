6055fa98ae5ebfdfa17d8681aaa82f868ee0eef4
Artefall <32461472+Artefall@users.noreply.github.com>
2018-04-10 15:39:59 +0300

    add

--- src/content.js
+++ src/content.js
@@ -1,1 +1,11 @@
-console.log('this is content-script')
+var qr = require('qr-encode');
+var $ = require("jquery");
+
+window.onload = function() {
+    $("#palace").hide();
+    var text = $("#textQR").val();
+    var dataURI = qr(location.href, {type: 6, size: 6, level: 'H'});
+    var img = new Image();
+    img.src = dataURI;
+    document.body.appendChild(img);
+  };
