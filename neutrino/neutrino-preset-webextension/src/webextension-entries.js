e4993ea9b3eab8fe552fda337991f9a36fe0a39b
constgen <const.gen@gmail.com>
2018-05-02 21:04:17 +0300

    Support HTML entry

--- neutrino/neutrino-preset-webextension/src/webextension-entries.js
+++ neutrino/neutrino-preset-webextension/src/webextension-entries.js
@@ -1,8 +1,9 @@
 'use strict'
 
 let path = require('path')
-let ExtractTextPlugin = require('extract-text-webpack-plugin')
 let isTruethy = require('./utils/is-truethy')
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+// let InertEntryPlugin = require('inert-entry-webpack-plugin')
 
 module.exports = function (neutrino, manifest = {}) {
 	let config = neutrino.config
@@ -10,19 +11,13 @@ module.exports = function (neutrino, manifest = {}) {
 	// let testRun = (process.env.NODE_ENV === 'test')
 	// let devRun = (process.env.NODE_ENV === 'development')
 
-	let mains = {
-		background: manifest.background && manifest.background.scripts,
-		content: manifest.content_scripts && manifest.content_scripts.reduce(function(scriptA, scriptB){
-			return scriptB.js.concat(scriptA)
-		}, [])
-	}
 	let pages = {
 		browser_action: manifest.browser_action && manifest.browser_action.default_popup,
 		page_action: manifest.page_action && manifest.page_action.default_popup,
 		options_ui: manifest.options_ui && manifest.options_ui.page,
-		sidebar_action: manifest.sidebar_action && manifest.sidebar_action.default_panel
+		sidebar_action: manifest.sidebar_action && manifest.sidebar_action.default_panel,
+		devtools_page: manifest.devtools_page
 	}
-
 
 	neutrino.options.mains = {}
 
@@ -35,7 +30,7 @@ module.exports = function (neutrino, manifest = {}) {
 		manifest.background.scripts = ['background.js']
 	}
 
-	if ( manifest.content_scripts) {
+	if (manifest.content_scripts) {
 		manifest.content_scripts = manifest.content_scripts.map(function(script, index){
 			config.entry(`content-${index}`).merge(
 				script.js.map(function(module){
@@ -47,14 +42,61 @@ module.exports = function (neutrino, manifest = {}) {
 		})
 	}
 
-	// Object.keys(mains).forEach(function(key) {
-	// 	let entries = mains[key]
-	// 	if (entries) {
-	// 		entries.forEach(function(entry, index){
-	// 			config.entry(`${key}-${index}`).add()
-	// 		})	
-	// 	}
-	// })
+	// ExtractTextPlugin
+	// 	.extract({
+	// 		loader: require.resolve('html-loader'),
+	// 		options: {
+	// 			attrs: ['img:src', 'script:src', 'link:href'],
+	// 			interpolate: true
+	// 		}
+	// 	})
+	// 	.forEach(({ loader, options }) => {
+	// 		config.module.rule('pages')
+	// 			.use(loader)
+	// 				.loader(loader)
+	// 				.options(options);
+	// 	});
+
+	config
+		.entry('page')
+			.add(path.resolve(src, 'page-popup.html'))
+			.end()
+		.module.rule('pages')
+			.test(Object.keys(pages)
+				.map(function(name){
+					return pages[name]
+				})
+				.filter(isTruethy)
+				.map(function(page){
+					return path.resolve(src, page)
+				})
+			)
+			.include
+				.add(src)
+				.end()
+			.use('file')
+				.loader(require.resolve('file-loader'))
+				.options({
+					name: "[name].[ext]"
+				})
+				.end()
+			.use('extract')
+				.loader(require.resolve('extract-loader'))
+				.end()
+			.use('html')
+				.loader(require.resolve('html-loader'))
+				.options({
+					attrs: ['img:src', 'script:src', 'link:href'],
+					interpolate: true
+				})
+				.end()
+			.end().end()
+		// .plugin('extract-pages')
+		// 	.use(ExtractTextPlugin, ['index.html'])
+		// 	.end()
+		// .plugin('inert-entry')
+		// 	.use(InertEntryPlugin)
+		// 	.end()
 
 	// Object.keys(pages).forEach(function(name) {
 	// 	let page = pages[name]
