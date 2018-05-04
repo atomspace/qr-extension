b42188b7cf8fdbea16f03fa3c7731efdbb1fb8c2
constgen <const.gen@gmail.com>
2018-05-04 16:33:28 +0300

    Fix absent import

--- src/content.js
+++ src/content.js
@@ -1,6 +1,9 @@
-require("./conrollers/.js");
-require("./conrollers/.js");
-require("./conrollers/.js")
+let messageChannel = require('./services/message-channel');
+
 messageChannel.onMessage('signal', function (data) {
 	console.log(data);
 });
+
+require("./conrollers/.js");
+require("./conrollers/.js");
+require("./conrollers/.js")
