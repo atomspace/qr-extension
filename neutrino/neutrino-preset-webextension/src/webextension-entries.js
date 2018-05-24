0f102529638fcb0b2289f63229384659ea35e731
constgen <const.gen@gmail.com>
2018-05-25 01:34:21 +0300

    Add support of HTML pages as entry points

--- neutrino/neutrino-preset-webextension/src/webextension-entries.js
+++ neutrino/neutrino-preset-webextension/src/webextension-entries.js
@@ -1,13 +1,11 @@
-'use strict'
+let path = require('path');
 
-let path = require('path')
-let isTruethy = require('./utils/is-truethy')
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-// let InertEntryPlugin = require('inert-entry-webpack-plugin')
+let isTruethy = require('./utils/is-truethy');
 
 module.exports = function (neutrino, manifest = {}) {
-	let config = neutrino.config
-	let src = neutrino.options.source
+	let config = neutrino.config;
+	let src = neutrino.options.source;
+
 	// let testRun = (process.env.NODE_ENV === 'test')
 	// let devRun = (process.env.NODE_ENV === 'development')
 
@@ -17,144 +15,85 @@ module.exports = function (neutrino, manifest = {}) {
 		options_ui: manifest.options_ui && manifest.options_ui.page,
 		sidebar_action: manifest.sidebar_action && manifest.sidebar_action.default_panel,
 		devtools_page: manifest.devtools_page
-	}
+	};
 
-	neutrino.options.mains = {}
+	let pagesPaths = Object.keys(pages)
+		.map(function (name) { return pages[name]; })
+		.filter(isTruethy)
+		.map(function (page) {
+			return path.resolve(src, page);
+		});
+
+	neutrino.options.mains = {};
 
 	if (manifest.background && manifest.background.scripts) {
 		config.entry('background').merge(
-			manifest.background.scripts.map(function(module){
-				return path.resolve(src, module)
+			manifest.background.scripts.map(function (module) {
+				return path.resolve(src, module);
 			})
-		)
-		manifest.background.scripts = ['background.js']
+		);
+		manifest.background.scripts = ['background.js'];
 	}
 
 	if (manifest.content_scripts) {
-		manifest.content_scripts = manifest.content_scripts.map(function(script, index){
+		manifest.content_scripts = manifest.content_scripts.map(function (script, index) {
 			config.entry(`content-${index}`).merge(
-				script.js.map(function(module){
-					return path.resolve(src, module)
+				script.js.map(function (module) {
+					return path.resolve(src, module);
 				})
-			)
-			script.js = [`content-${index}.js`]
-			return script
-		})
+			);
+			script.js = [`content-${index}.js`];
+			return script;
+		});
 	}
 
-	// ExtractTextPlugin
-	// 	.extract({
-	// 		loader: require.resolve('html-loader'),
-	// 		options: {
-	// 			attrs: ['img:src', 'script:src', 'link:href'],
-	// 			interpolate: true
-	// 		}
-	// 	})
-	// 	.forEach(({ loader, options }) => {
-	// 		config.module.rule('pages')
-	// 			.use(loader)
-	// 				.loader(loader)
-	// 				.options(options);
-	// 	});
+	pagesPaths.forEach(function (pagePath) {
+		config.entry('entry-pages').add(pagePath);
+	});
 
-	config
-		.entry('page')
-			.add(path.resolve(src, 'page-popup.html'))
-			.end()
-		.module.rule('pages')
-			.test(Object.keys(pages)
-				.map(function(name){
-					return pages[name]
-				})
-				.filter(isTruethy)
-				.map(function(page){
-					return path.resolve(src, page)
-				})
-			)
-			.include
-				.add(src)
-				.end()
-			.use('file')
-				.loader(require.resolve('file-loader'))
-				.options({
-					name: "[name].[ext]"
-				})
-				.end()
-			.use('extract')
-				.loader(require.resolve('extract-loader'))
-				.end()
-			.use('html')
-				.loader(require.resolve('html-loader'))
-				.options({
-					attrs: ['img:src', 'script:src', 'link:href'],
-					interpolate: true
-				})
-				.end()
-			.end().end()
-		// .plugin('extract-pages')
-		// 	.use(ExtractTextPlugin, ['index.html'])
-		// 	.end()
-		// .plugin('inert-entry')
-		// 	.use(InertEntryPlugin)
-		// 	.end()
+	config.when(pagesPaths.length, function () {
+		config
+			.module.rule('entry-pages')
+				.test(pagesPaths)
+				.include
+					.add(src)
+					.end()
+				.use('file')
+					.loader(require.resolve('file-loader'))
+					.options({
+						name: '[name].[ext]'
+					})
+					.end()
+				.use('extract')
+					.loader(require.resolve('extract-loader'))
+					.end()
+				.use('html')
+					.loader(require.resolve('html-loader'))
+					.options({
+						attrs: ['img:src', 'script:src', 'link:href'],
+						interpolate: true
+					})
+					.end()
+				.use('post-html')
+					.loader(require.resolve('posthtml-loader'))
+					.options({
+						plugins: [
+							require('posthtml-shorten')({
+								shortener: {
+									process (url) {
+										return Promise.resolve(url.replace(
+											url,
+											`\${require('spawn-loader!./${url}')}`
+										));
+									}
+								},
+								tags: ['script'],
+								attributes: ['src']
+							})
+						]
+					})
+					.end()
+				.end().end();
+	});
 
-	// Object.keys(pages).forEach(function(name) {
-	// 	let page = pages[name]
-	// 	if (page) {
-	// 		let extractPlugin = new ExtractTextPlugin(page)
-
-	// 		config
-	// 			.entry(name)
-	// 				.add(path.resolve(src, page))
-	// 				.end()
-	// 			.module.rule(`page-${name}`)
-	// 				.test(path.resolve(src, page))
-	// 				.use('extract')
-	// 					.loader(extractPlugin.extract({
-	// 						loader: require.resolve('html-loader'),
-	// 						options: {
-	// 							attrs: ['img:src', 'script:src', 'link:href'],
-	// 							interpolate: false
-	// 						}
-	// 					}))
-	// 					.end()
-	// 				.end()
-	// 			.end()
-	// 			.plugin(`extract-${name}`)
-	// 				.use(function(){ return extractPlugin })
-	// 			.end()
-	// 	}
-	// })
-
-	// config.module.rule('pages')
-	// 	.test(Object.keys(pages)
-	// 		.map(function(name){
-	// 			return pages[name]
-	// 		})
-	// 		.filter(isTruethy)
-	// 		.map(function(page){
-	// 			return path.resolve(src, page)
-	// 		})
-	// 	)
-	// 	.include
-	// 		.add(src)
-	// 		.end()
-	// 	.use('file')
-	// 		.loader(require.resolve('file-loader'))
-	// 		.options({
-	// 			name: "[name].[ext]"
-	// 		})
-	// 		.end()
-	// 	.use('extract')
-	// 		.loader(require.resolve('extract-loader'))
-	// 		.end()
-	// 	.use('html')
-	// 		.loader(require.resolve('html-loader'))
-	// 		.options({
-	// 			attrs: ['img:src', 'script:src', 'link:href'],
-	// 			interpolate: false
-	// 		})
-	// 		.end()
-
-
-}
+};
