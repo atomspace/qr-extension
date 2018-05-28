bb7eaee01c67852a4927bafebe31cfe7a1149d6c
Artefall <32461472+Artefall@users.noreply.github.com>
2018-05-28 03:14:28 +0300

    It's just work

--- src/services/img-manipulator.js
+++ src/services/img-manipulator.js
@@ -11,43 +11,26 @@ module.exports = {
 	append (text) {
 		qrBackground = $('<div>');
 
-		qrBackground.style.position = 'fixed';
+
 		$(qrBackground).css({
 			position: 'fixed', width: '100%', height: '100%',
 			top: '0', zIndex: '100', backgroundColor: 'rgba(0, 0, 0, 0.5)'
 		});
-
-		// qrBackground.style.width = '100%';
-		// qrBackground.style.height = '100%';
-		// qrBackground.style.top = '0';
-		// qrBackground.style.zIndex = '100';
-		// qrBackground.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-
 		this.img.src = qrGenerator.generate(text);
-		let qrImage = this.img;
 
-		$(qrImage).css({
+
+		$(this.img).css({
 			position: 'relative', left: '50%', top: '50%', marginLeft: '-185px',
 			marginTop: '-185px'
 		});
 
-		// this.img.style.position = 'relative';
-		// this.img.style.left = '50%';
-		// this.img.style.top = '50%';
-		// this.img.style.marginLeft = '-185px';
-		// this.img.style.marginTop = '-185px';
+		$(qrBackground).append(this.img);
 
-
-		qrBackground.appendChild(qrImage);
-
-		// qrBackground.setAttribute('onclick', 'remove()');
-		// document.body.appendChild(qrBackground);
 		$('body').append(qrBackground);
 	},
 	remove () {
 		$('body').on('click', function () {
-			// qrBackground.remove(qrBackground);
-			qrBackground.remove();
+			$(qrBackground).remove();
 		});
 	}
 };
