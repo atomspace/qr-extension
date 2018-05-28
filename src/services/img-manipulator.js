fd05cd4e6f6dc078ee502c7d8d6b18205163f291
Artefall <32461472+Artefall@users.noreply.github.com>
2018-05-28 04:54:52 +0300

    Bug fixed

--- src/services/img-manipulator.js
+++ src/services/img-manipulator.js
@@ -3,34 +3,41 @@ let $ = require('jquery');
 let qrGenerator = require('./qr-generator');
 
 let qrBackground;
-
+let isOn = false;
 
 module.exports = {
 	img: new Image(),
 
 	append (text) {
-		qrBackground = $('<div>');
+		if (isOn === false) {
+			isOn = true;
+			qrBackground = $('<div>');
+			let bcWidth = $(window).width();
+			let bcHeight = $(window).height();
+
+			$(qrBackground).css({
+				position: 'fixed', width: bcWidth, height: bcHeight,
+				top: '0', zIndex: '100', backgroundColor: 'rgba(0, 0, 0, 0.5)'
+			});
+			this.img.src = qrGenerator.generate(text);
 
 
-		$(qrBackground).css({
-			position: 'fixed', width: '100%', height: '100%',
-			top: '0', zIndex: '100', backgroundColor: 'rgba(0, 0, 0, 0.5)'
-		});
-		this.img.src = qrGenerator.generate(text);
+			$(this.img).css({
+				position: 'relative', left: '50%', top: '50%', marginLeft: '-185px',
+				marginTop: '-185px'
+			});
 
+			$(qrBackground).append(this.img);
 
-		$(this.img).css({
-			position: 'relative', left: '50%', top: '50%', marginLeft: '-185px',
-			marginTop: '-185px'
-		});
-
-		$(qrBackground).append(this.img);
-
-		$('body').append(qrBackground);
+			$('body').append(qrBackground);
+		}
 	},
 	remove () {
-		$('body').on('click', function () {
-			$(qrBackground).remove();
-		});
+		if (isOn === true) {
+			$('body').on('click', function () {
+				$(qrBackground).remove();
+				isOn = false;
+			});
+		}
 	}
 };
